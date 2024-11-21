7452d203ed93445512f2d33c5ad86cbf67850684
osama-mhmd <docosmo010@gmail.com>
2024-11-21 13:03:48 +0200

    hash-object

--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,4 @@
+import * as crypto from "node:crypto";
 import * as fs from "fs";
 import * as zlib from "node:zlib";
 
@@ -7,6 +8,7 @@ const command = args[0];
 enum Commands {
   Init = "init",
   CatFile = "cat-file",
+  HashObject = "hash-object",
 }
 
 switch (command) {
@@ -28,11 +30,24 @@ switch (command) {
       `.git/objects/${args[2].slice(0, 2)}/${args[2].slice(2)}`
     );
     zlib.unzip(buffer, (_, buf) => {
-      // console.log(buf.toString().trim());
       const [typeAndSize, content] = buf.toString().split("\0");
       const [type, size] = typeAndSize.split(" ");
       process.stdout.write(content);
     });
+    break;
+  case Commands.HashObject:
+    if (args[1] !== "-w") break; // Checking for the "-w" flag
+    if (!args[2]) break; // Checking for the file name
+    const buf = fs.readFileSync(args[2]);
+    const hash = crypto.createHash("sha1");
+    hash.update(buf);
+    const name = hash.digest("hex");
+    process.stdout.write(name);
+    fs.mkdirSync(`.git/objects/${name.slice(0, 2)}`, { recursive: true });
+    fs.writeFileSync(
+      `.git/objects/${name.slice(0, 2)}/${name.slice(2)}`,
+      zlib.deflateSync(Buffer.from(`blob ${buf.byteLength}\0${buf.toString()}`))
+    );
     break;
   default:
     throw new Error(`Unknown command ${command}`);
